1f714402037a926c8c7643bdafd75ee33738c1e3
Derek Wan <d.wan@icloud.com>
2021-01-12 14:19:14 +0800

    Don't run prettier on jsx, then let tsc auto compile

--- academind--typescript-course-for-beginners-2020/using-ts.js
+++ academind--typescript-course-for-beginners-2020/using-ts.js
@@ -1,9 +1,9 @@
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
-function add(num1, num2) {
-  return num1 + num2;
+function add(num1, num2, num3) {
+    return num1 + num2 + num3;
 }
 button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
+    console.log(add(+input1.value, +input2.value, 1));
 });
