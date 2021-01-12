b4236b3c566d00eaef0a4377fe555450fdc9c238
Derek Wan <d.wan@icloud.com>
2021-01-12 21:45:03 +0800

    Start working on mozilla-js tutorial

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-console.log("Welcome to app.ts!");
-
-const x = 1;
-const y = "arst";
-
-console.log(x + y);
---
+++ src/mozilla-js/main.ts
@@ -0,0 +1,9 @@
+console.log("Hello from main.ts");
+
+const myHeading = document.querySelector("h1");
+
+if (myHeading !== null) {
+  myHeading.textContent = "Hello world!";
+
+  alert("hello!");
+}
