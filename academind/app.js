c9c268c14b65689007be5790d3f0a6f82cd533b9
Derek Wan <d.wan@icloud.com>
2021-01-12 16:45:28 +0800

    Rename original to 00 - introduction

--- academind/app.js
+++ academind/app.js
@@ -1,9 +1,7 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2, num3) {
-    return num1 + num2 + num3;
+function add(n1, n2) {
+    return n1 + n2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value, 1000));
-});
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- academind/app.ts
+++ academind/app.ts
@@ -1,11 +1,8 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1: number, num2: number, num3: number): number {
-  return num1 + num2 + num3;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value, 1000));
-});
+const number1 = 5;
+const number2 = 2.8;
+const result = add(number1, number2);
+console.log(result);
