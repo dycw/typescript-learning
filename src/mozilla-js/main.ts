d3e2353661f6755d57a063bbc30dd20dbbce068f
Derek Wan <d.wan@icloud.com>
2021-01-12 22:06:05 +0800

    Can do image changing

--- src/mozilla-js/main.ts
+++ src/mozilla-js/main.ts
@@ -3,8 +3,7 @@ console.log("Hello from main.ts");
 const myHeading = document.querySelector("h1");
 
 if (myHeading !== null) {
-  console.log("Changing h1");
-  myHeading.textContent = "Hello world!";
+  myHeading.textContent = "Hello world! (set by script)";
 
   // alert("hello!");
 }
@@ -14,7 +13,22 @@ const myHtml = document.querySelector("html");
 if (myHtml !== null) {
   console.log("Adding onClick to HTML");
 
-  myHtml.onclick = function () {
-    alert("Ouch! Stop poking me!");
+  // myHtml.onclick = function () {
+  //   alert("Ouch! Stop poking me!");
+  // };
+}
+
+const myImage = document.querySelector("img");
+
+if (myImage !== null) {
+  myImage.onclick = function () {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "src/mozilla-js/images/firefox-icon.png") {
+      console.log("Setting path 1");
+      myImage.setAttribute("src", "src/mozilla-js/images/firefox2.png");
+    } else {
+      console.log("Setting path 2");
+      myImage.setAttribute("src", "src/mozilla-js/images/firefox-icon.png");
+    }
   };
 }
