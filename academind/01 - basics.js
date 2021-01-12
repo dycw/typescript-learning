5ba9bfaa4f6f6ff1d00536e93244487e6b840339
Derek Wan <d.wan@icloud.com>
2021-01-12 19:58:18 +0800

    Move *.ts into src/, output is dist/, ignore *.js

--- academind/01 - basics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function add_01(n1, n2) {
-    return n1 + n2;
-}
-const number1 = 5;
-const number2 = 2.8;
-const result = add_01(number1, number2);
-console.log(result);
--- academind/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log("Welcome to app.ts!");
-const x = 1;
-const y = "arst";
-console.log(x + y);
