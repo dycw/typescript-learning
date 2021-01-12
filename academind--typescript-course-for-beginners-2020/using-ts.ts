9f495a0cde4ada585cf2ea7d2b563ff223971630
Derek Wan <d.wan@icloud.com>
2021-01-12 10:45:03 +0800

    Add eslint

--- academind--typescript-course-for-beginners-2020/using-ts.ts
+++ academind--typescript-course-for-beginners-2020/using-ts.ts
@@ -2,7 +2,7 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number): string {
+function add(num1: number, num2: number): number {
   return num1 + num2;
 }
 
