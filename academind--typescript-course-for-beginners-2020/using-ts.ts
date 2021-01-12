125dca43d163eedbbfed29ff5c445023a0324c4d
Derek Wan <d.wan@icloud.com>
2021-01-12 12:46:49 +0800

    Revert "foo"
    This reverts commit f0c6caaad20c13860e359843674defe145f75778.

--- academind--typescript-course-for-beginners-2020/using-ts.ts
+++ academind--typescript-course-for-beginners-2020/using-ts.ts
@@ -3,7 +3,7 @@ const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1: number, num2: number): number {
-  return num1 + True + "a" + num2;
+  return num1 + "a" + num2;
 }
 
 button.addEventListener("click", function () {
