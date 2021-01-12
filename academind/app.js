5f45cc95c32fb4b6ef0d60c36b5d89c037bb9f03
Derek Wan <d.wan@icloud.com>
2021-01-12 16:58:40 +0800

    Add tuple

--- academind/app.js
+++ academind/app.js
@@ -1,5 +1,11 @@
 var person = {
     name: "Derek",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby, hobby.toUpperCase());
+}
--- academind/app.ts
+++ academind/app.ts
@@ -1,6 +1,17 @@
-const person: { name: string; age: number } = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Derek",
   age: 30,
+  hobbies: ["Sports", `Cooking`],
+  role: [2, "author"],
 };
 
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby, hobby.toUpperCase());
+}
