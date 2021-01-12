90b509122201cc3ca14b87f0c57d08145d1c4f19
Derek Wan <d.wan@icloud.com>
2021-01-12 17:48:07 +0800

    Pull out 04

---
+++ academind/04 - functions.js
@@ -0,0 +1,20 @@
+function add_04(n1, n2) {
+    return n1 + n2;
+}
+console.log(add_04(1, 2));
+function printResult(num) {
+    console.log("Result: " + num);
+}
+printResult(add_04(5, 12));
+console.log("This is undefined: ", printResult(add_04(5, 12)));
+function returnsUndefined() {
+    return; // must have empty return as type -> undefined
+}
+console.log("This is also undefined:", returnsUndefined());
+var add_copy = add_04;
+console.log(add_copy(2, 3));
+function addAndHandle(n1, n2, cb) {
+    var result = add_04(n1, n2);
+    cb(result);
+}
+addAndHandle(5, 6, console.log);
--- academind/app.js
+++
@@ -1,8 +0,0 @@
-function add_04(n1, n2) {
-    return n1 + n2;
-}
-console.log(add_04(1, 2));
-function printResult(num) {
-    console.log("Result: " + num);
-}
-printResult(add_04(5, 12));
