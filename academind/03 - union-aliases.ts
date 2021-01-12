b4292e1dd5eadc5be140e50ada64c08370453737
Derek Wan <d.wan@icloud.com>
2021-01-12 17:13:25 +0800

    Save 03 - union-aliases

---
+++ academind/03 - union-aliases.ts
@@ -0,0 +1,24 @@
+type Combinable = number | string;
+
+function add_03(
+  n1: Combinable,
+  n2: Combinable,
+  conv: "as-number" | "as-string"
+): number | string {
+  let result;
+  if (typeof n1 === "number" && typeof n2 === "number") {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  }
+  if (conv === "as-number") {
+    return +result; // same as parseFloat(result)
+  } else {
+    return result.toString();
+  }
+}
+
+console.log(add_03(1, 2, "as-number"));
+console.log(add_03(1, 2, "as-string"));
+console.log(add_03("11", "22", "as-number"));
+console.log(add_03("str1", "str2", "as-string"));
