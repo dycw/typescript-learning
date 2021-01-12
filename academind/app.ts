c5affdf3e5e33070a42ad855c0fbfc2b0e045d45
Derek Wan <d.wan@icloud.com>
2021-01-12 17:00:35 +0800

    Add Enums

--- academind/app.ts
+++ academind/app.ts
@@ -1,13 +1,21 @@
+enum ROLE2 {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
   role: [number, string];
+  role_2: ROLE2;
 } = {
   name: "Derek",
   age: 30,
   hobbies: ["Sports", `Cooking`],
   role: [2, "author"],
+  role_2: ROLE2.ADMIN,
 };
 
 console.log(person);
