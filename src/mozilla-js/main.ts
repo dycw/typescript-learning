728553d8e610318ce43a8e310244dab1d5d7439c
Derek Wan <d.wan@icloud.com>
2021-01-12 22:00:54 +0800

    Copy the index/etc from mozilla example

--- src/mozilla-js/main.ts
+++ src/mozilla-js/main.ts
@@ -3,7 +3,18 @@ console.log("Hello from main.ts");
 const myHeading = document.querySelector("h1");
 
 if (myHeading !== null) {
+  console.log("Changing h1");
   myHeading.textContent = "Hello world!";
 
-  alert("hello!");
+  // alert("hello!");
+}
+
+const myHtml = document.querySelector("html");
+
+if (myHtml !== null) {
+  console.log("Adding onClick to HTML");
+
+  myHtml.onclick = function () {
+    alert("Ouch! Stop poking me!");
+  };
 }
