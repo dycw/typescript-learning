22a676d2c161909a411ccaedc3ae0662e6aa9eae
Derek Wan <d.wan@icloud.com>
2021-01-12 17:34:32 +0800

    Update

---
+++ academind/app.ts
@@ -0,0 +1,11 @@
+function add_04(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+console.log(add_04(1, 2));
+
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
+
+printResult(add_04(5, 12));
